f023734a845623329a39c9f5852c2e060c767d68
Ibrahim <ibrahimbhat053@gmail.com>
2024-08-24 21:13:26 +0530

    delted

--- partials/_logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) {  
-    session_start();
-}
-echo "<h2>Logging you out. Please wait...</h2>";
-
-session_destroy();
-header("Location: /proj%20kashmir/kashmir_website/index.php ");
-exit();
